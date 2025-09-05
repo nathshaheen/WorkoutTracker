c72a124727c215fde6d5032045897560a9616030
Nathan Shaheen <nathshaheen1@gmail.com>
2025-09-05 19:40:33 +1000

    Initial Commit

---
+++ WorkoutTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WorkoutTracker
+//
+//  Created by Nathan Shaheen on 5/9/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
