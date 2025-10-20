47a0acab875e84fb1c42ce0e0597a15e7680b469
Nathan Shaheen <nathshaheen1@gmail.com>
2025-10-20 18:50:23 +1100

    Initial Commit

---
+++ WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by Nathan Shaheen on 20/10/2025.
+//
+
+import Testing
+@testable import WorkoutTracker
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
